5a36530203402a6ba3288280f0f05b1ec7670dbc
Citamehtam <art75322@gmail.com>
2018-03-15 14:16:14 +0800

    3rd ver.

--- hw1.cpp
+++ hw1.cpp
@@ -7,24 +7,39 @@
 int main()
 {
 	int m=1,n=1;
-	int N=pow(10,9);   //10^9
-	clock_t t1,t2;
+	int N=pow(10,9);
+	printf ("N = %d\n",N);
+	clock_t t1,t2,T1,T2;
 	// addition
     t1 = clock();
     for (int i=0;i<N;i++){
-    	m=n+20;
+    	m=n+200;
 	}
     t2 = clock();
-    printf("time for addition= %.3f s\n", (t2-t1)/(double)(CLOCKS_PER_SEC));
+    
+    T1 = clock();
+    for (int i=0;i<N;i++){
+    	m=n+200;
+    	m=n+200;
+	}
+    T2 = clock();
+    printf("time for addition(N times)= %.3f s\n", ((T2-T1)-(t2-t1))/(double)(CLOCKS_PER_SEC));
     //printf("m=%d \nn=%d\n",m,n);
 
 	// multiple
     t1 = clock();
     for (int i=0;i<N;i++){
-    	m=n*20;
+    	m=n*200;
 	}  
     t2 = clock();
-    printf("time for multiple= %.3f s\n", (t2-t1)/(double)(CLOCKS_PER_SEC));
+    
+    T1 = clock();
+    for (int i=0;i<N;i++){
+    	m=n*200;
+    	m=n*200;
+	}
+    T2 = clock();
+    printf("time for multiple(N times)= %.3f s\n", ((T2-T1)-(t2-t1))/(double)(CLOCKS_PER_SEC));
 
 	// sine function
     t1 = clock();
@@ -32,7 +47,14 @@ int main()
     	m=sin(n);
 	}
     t2 = clock();
-    printf("time for sine function= %.3f s\n", (t2-t1)/(double)(CLOCKS_PER_SEC));
+    
+    T1 = clock();
+    for (int i=0;i<N;i++){
+    	m=sin(n);
+    	m=sin(n);
+	}
+    T2 = clock();
+    printf("time for sine function(N times)= %.3f s\n", ((T2-T1)-(t2-t1))/(double)(CLOCKS_PER_SEC));
     return 0;
-    //test for 2nd ver. 
+
 }
